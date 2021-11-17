5e07f50b0a7829722a2b2e2c398a7f5223fa2d69
Ilya Kerymov <illya.kerymov31@gmail.com>
2021-11-17 14:50:13 +0200

    Add OriginaryCalc.

--- myApp/Program.cs
+++ myApp/Program.cs
@@ -2,11 +2,38 @@
 
 namespace myApp
 {
+    class OriginaryCalc
+    {
+        public double num1 { get; set; }
+        public double num2 { get; set; }
+
+        public OriginaryCalc() { }
+
+        public double add() { 
+            return num1 + num2;
+        }
+
+        public double sub() { 
+            return num1 - num2;
+        }
+
+        public double mul() { 
+            return num1 * num2;
+        }
+
+        public double div() { 
+            return num1 / num2;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {   
-
+            OriginaryCalc calc = new OriginaryCalc();
+            calc.num1 = 3;
+            calc.num2 = 7;
+            Console.WriteLine(calc.add());
         }
     }
 }
