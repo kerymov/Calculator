1afd8faa5d0fdd669c09b1470a1559821c464e03
Yat08 <kostya.yurchuk.git.999@gmail.com>
2021-12-01 15:35:52 +0200

    Add Abstract Calc

---
+++ myApp/AbstractCalc.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace myApp
+{
+    abstract class AbstractCalc
+    {
+        public abstract double add();
+
+        public abstract double sub();
+
+        public abstract double mul();
+
+        public abstract double div();
+    }
+
+}
